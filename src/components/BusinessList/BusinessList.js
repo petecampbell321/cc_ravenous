009b223b34aaac6a4f9c51bdaa1778e793c44a9c
Pete Campbell <petecampbell@petes-mbp.home>
2018-02-01 14:35:56 +0000

    Fixed typo for searchYelp function

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,8 +2,7 @@ import React from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business.js';
 
-class BusinessList extends React.Component 
-{
+class BusinessList extends React.Component {
     render() {
         return(
             <div className="BusinessList">
